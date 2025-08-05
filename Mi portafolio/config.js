1eb9140703115b2f659a523e90f6d3197220bc69
Alfred Uribe <alfreduriberolong20@gmail.com>
2025-08-05 17:46:50 -0500

    avance en el portafolio

---
+++ Mi portafolio/config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            animation: {
+                'gradient': 'gradient 8s ease infinite',
+                'fadeInUp': 'fadeInUp 1s ease forwards',
+                'bounce-slow': 'bounce 3s infinite',
+            },
+            keyframes: {
+                gradient: {
+                    '0%, 100%': {
+                        'background-size': '200% 200%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size': '200% 200%',
+                        'background-position': 'right center'
+                    }
+                },
+                fadeInUp: {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(30px)'
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0px)'
+                    }
+                }
+            }
+        }
+    }
+}
